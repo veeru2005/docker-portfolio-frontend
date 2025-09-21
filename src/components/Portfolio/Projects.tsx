9a3998e47925e4e5fc363a5911d9a5df19a377d6
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-21 19:29:04 +0530

    Update Projects.tsx

--- src/components/Portfolio/Projects.tsx
+++ src/components/Portfolio/Projects.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Box, Typography, Container, Card, CardMedia, CardContent, CardActions, Button, Chip, IconButton } from '@mui/material';
 import { Launch as LaunchIcon, GitHub as GitHubIcon } from '@mui/icons-material';
 
-const API_BASE_URL = 'http://13.61.16.218:8083/portfolio_back1';
+const API_BASE_URL = 'http://13.62.51.135:8083/portfolio_back1';
 
 interface Project {
   id: number;
