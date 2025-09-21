729ed912ef8e751ae4313970510299872e279582
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-21 17:49:51 +0530

    Update AdminProjects.tsx

--- src/pages/AdminProjects.tsx
+++ src/pages/AdminProjects.tsx
@@ -26,7 +26,7 @@ import {
   GitHub as GitHubIcon
 } from '@mui/icons-material';
 
-const API_BASE_URL = 'http://56.228.10.81:8083/portfolio_back1';
+const API_BASE_URL = 'http://13.62.51.42:8083/portfolio_back1';
 
 interface Project {
   id: number;
