9882317f398cfc0b7cb29e87bfbcd7d4f668427b
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-21 18:09:26 +0530

    Update API base URL for AdminOTP component

--- src/pages/AdminOTP.tsx
+++ src/pages/AdminOTP.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import ArrowBack from '@mui/icons-material/ArrowBack';
 import MarkEmailRead from '@mui/icons-material/MarkEmailRead';
 
-const API_BASE_URL = 'http://13.62.51.42:8083/portfolio_back1';
+const API_BASE_URL = 'http://51.20.105.206:8083/portfolio_back1';
 
 const AdminOTP: React.FC = () => {
   const navigate = useNavigate();
