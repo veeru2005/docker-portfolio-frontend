f2ec4af5cf17bacc07d49115a61cc8bdc9561b40
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-21 18:09:39 +0530

    Update API_BASE_URL to new server address

--- src/pages/AdminProjects.tsx
+++ src/pages/AdminProjects.tsx
@@ -26,7 +26,7 @@ import {
   GitHub as GitHubIcon
 } from '@mui/icons-material';
 
-const API_BASE_URL = 'http://13.62.51.42:8083/portfolio_back1';
+const API_BASE_URL = 'http://51.20.105.206:8083/portfolio_back1';
 
 interface Project {
   id: number;
