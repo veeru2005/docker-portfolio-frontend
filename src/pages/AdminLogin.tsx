edb9681924e37e6bf407e8b770d216739e24caa9
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-21 19:40:32 +0530

    Update API base URL for AdminLogin

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -19,7 +19,7 @@ import Person from '@mui/icons-material/Person';
 import Email from '@mui/icons-material/Email';
 import Lock from '@mui/icons-material/Lock';
 
-const API_BASE_URL = 'http://13.62.51.135:8083/portfolio_back1';
+const API_BASE_URL = 'http://13.62.58.39:8083/portfolio_back1';
 
 const AdminLogin: React.FC = () => {
   const navigate = useNavigate();
