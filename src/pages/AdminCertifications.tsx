2422bae13be421dd685752349f81113286b84a07
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-22 18:57:59 +0530

    Update AdminCertifications.tsx

--- src/pages/AdminCertifications.tsx
+++ src/pages/AdminCertifications.tsx
@@ -8,7 +8,7 @@ import AdminNavbar from './AdminNavbar';
 
 
 
-const API_BASE_URL = 'http://16.171.65.245:8083/portfolio_back1';
+const API_BASE_URL = 'http://54.82.126.77:8083/portfolio_back1';
 
 const getAuthToken = () => localStorage.getItem('jwtToken') || '';
 
