5b398700ac2eaf372971e5999d17ed5982a77449
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-10-11 09:41:47 +0530

    Update EducationCertifications.tsx

--- src/components/Portfolio/EducationCertifications.tsx
+++ src/components/Portfolio/EducationCertifications.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Container, Typography, Card, CardContent, CardMedia, CircularProgress } from '@mui/material';
 
-const API_BASE_URL = 'http://35.172.220.252:8083/portfolio_back1';
+const API_BASE_URL = 'http://13.217.228.167:8083/portfolio_back1';
 
 // --- Interface updated to match the final backend entity ---
 interface Certification {
